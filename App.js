aa04ee05ac8b6ada264fdfc398b4c935005cc99f
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-15 13:26:45 -0700

    setting up expo and eslint

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{height: 100, width: 100, backgroundColor: 'orange'}}>
+       <Text>Hello from react native</Text>
+       
     </View>
   );
 }
@@ -16,5 +16,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
