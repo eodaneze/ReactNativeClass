3f6fd061880c732e178dcb649f3083d92a2a4065
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-15 14:41:32 -0700

    adding contents and more styles

--- App.js
+++ App.js
@@ -3,18 +3,58 @@ import {View, Text, SafeAreaView, StyleSheet} from 'react-native'
 
 const App = () => {
   return(
-    <SafeAreaView>
+    <SafeAreaView style={styles.wrapper}>
 
     <View style={styles.container}>
-      <Text>Current weather</Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+
+          <Text style={styles.highLow}>High: 8 </Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
     </View>
+      <View style={styles.bodyWrapper}>
+          <Text style={styles.description}>Its sunny</Text>
+          <Text style={styles.message}>Its perfect t-shirt weather</Text>
+      </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container:{
-    backgroundColor: 'pink'
+    flex: 1,
+    alignItems: 'center'
+  },
+  wrapper:{
+    backgroundColor: 'pink',
+    flex: 1
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels:{
+    fontSize: 30,
+    color: 'black'
+  },
+  highLow:{
+    fontSize: 20,
+    color: 'black'
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper:{
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  description:{
+    fontSize: 40, 
+  },
+  message: {
+    fontSize: 30
   }
 })
 export default App
