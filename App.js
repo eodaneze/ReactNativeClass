d49f68a07ceef6e2bb1cc01de88e2aa72a600554
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-15 13:42:21 -0700

    cleaning up the app and starting from ground

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {View, Text, SafeAreaView} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={{height: 100, width: 100, backgroundColor: 'orange'}}>
-       <Text>Hello from react native</Text>
-       
+const App = () => {
+  return(
+    <SafeAreaView>
+
+    <View>
+      <Text>Current weather</Text>
     </View>
-  );
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-});
+export default App
