a7f5224fc87a213837555af85179378c01562982
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-15 14:46:29 -0700

    adding a sun icon fro expo victor icon

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {View, Text, SafeAreaView, StyleSheet} from 'react-native'
+import { Feather } from '@expo/vector-icons';
 
 const App = () => {
   return(
     <SafeAreaView style={styles.wrapper}>
 
     <View style={styles.container}>
+      <Feather name="sun" size={108} color="black" />
         <Text style={styles.temp}>6</Text>
         <Text style={styles.feels}>Feels like 5</Text>
         <View style={styles.highLowWrapper}>
@@ -25,7 +27,8 @@ const App = () => {
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   wrapper:{
     backgroundColor: 'pink',
