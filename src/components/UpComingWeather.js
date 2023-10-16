f92eb2c55d2b45aa7ce00a1b39d8aba9f40f512d
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-16 06:04:44 -0700

    adding styles to the flatlist

--- src/components/UpComingWeather.js
+++ src/components/UpComingWeather.js
@@ -43,11 +43,11 @@ const DATA = [
 const Item = (props) => {
   const {dt_txt, min, max, condition} = props
   return(
-   <View>
+   <View style={styles.item}>
     <Feather name={'sun'} size={50} color={'white'}/>
-      <Text>{dt_txt}</Text>
-      <Text>{min}</Text>
-      <Text>{max}</Text>
+      <Text style={styles.date}>{dt_txt}</Text>
+      <Text style={styles.temp}>{min}</Text>
+      <Text style={styles.temp}>{max}</Text>
    </View>
   )
 }
@@ -71,6 +71,25 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         marginTop: StatusBar.currentHeight || 0,
+        backgroundColor: 'red'
+    },
+    item:{
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      borderWidth: 5,
+      backgroundColor: 'pink'
+    },
+    temp:{
+        color: 'white',
+        fontSize: 20
+    },
+    date:{
+      color: 'white',
+      fontSize: 15,
     }
 })
 export default UpComingWeather
