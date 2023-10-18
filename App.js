dced8acaecbd98994a39e1f3a1d2a03b0b58cfed
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-18 14:25:03 -0700

    adding natigations using react-navigator

--- App.js
+++ App.js
@@ -3,11 +3,20 @@ import {View, StyleSheet} from 'react-native'
 import CurrentWeather from './src/screens/CurrentWeather';
 import UpComingWeather from './src/screens/UpComingWeather';
 import City from './src/screens/City';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator()
 const App = () => {
+
   return(
-     <View style={Styles.container}>
-           <CurrentWeather />
-     </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+         <Tab.Screen name={'Current'} component={CurrentWeather}/>
+         <Tab.Screen name={'Upcoming'} component={UpComingWeather}/>
+         <Tab.Screen name={'City'} component={City}/>
+       </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -4,17 +4,18 @@ import { Feather } from '@expo/vector-icons';
 import RowText from '../components/RowText';
 
 const CurrentWeather = () => {
+  const{wrapper, container, temp, feels, highLowWrapper, highLow, bodyWrapper, description, message} = styles
   return (
-    <SafeAreaView style={styles.wrapper}>
+    <SafeAreaView style={wrapper}>
 
-      <View style={styles.container}>
+      <View style={container}>
         <Feather name="sun" size={108} color="black" />
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
-        <RowText messageOne={'High: 8'} messageTwo={'Low: 6'} containerStyles={styles.highLowWrapper} messageOneStyles={styles.highLow} messageTwoStyles={styles.highLow} />
+        <Text style={temp}>6</Text>
+        <Text style={feels}>Feels like 5</Text>
+        <RowText messageOne={'High: 8'} messageTwo={'Low: 6'} containerStyles={highLowWrapper} messageOneStyles={highLow} messageTwoStyles={highLow} />
 
       </View>
-      <RowText messageOne={'Its sunny'} messageTwo={'Its perfect t-shirt weather'} containerStyles={styles.bodyWrapper} messageOneStyles={styles.description} messageTwoStyles={styles.message} />
+      <RowText messageOne={'Its sunny'} messageTwo={'Its perfect t-shirt weather'} containerStyles={bodyWrapper} messageOneStyles={description} messageTwoStyles={message} />
     </SafeAreaView>
   )
 }
@@ -55,7 +56,7 @@ const styles = StyleSheet.create({
     fontSize: 40,
   },
   message: {
-    fontSize: 30
+    fontSize: 25
   }
 })
 export default CurrentWeather
