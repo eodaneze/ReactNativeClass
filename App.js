40000e3f8218b9652fec97aed54acc9df87448fa
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-19 09:23:07 -0700

    setting up a loading spinner using the activity indicator

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { View, ActivityIndicator, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
-import Counter from './src/demo/Counter';
-// const Tab = createBottomTabNavigator()
 const App = () => {
-
+  const[loading, setLoading] = useState(true);
+  if (loading){
+    return (
+      <View style={styles.container}>
+          <ActivityIndicator size={'large'} color={'blue'}/>
+      </View>
+    )
+  }
   return(
     <NavigationContainer>
-       <Counter />
+       <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+   container:{
+    justifyContent: 'center',
+    flex: 1
+   }
+})
 
 export default App
