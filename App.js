21cb1b2701f7c92cbce2e12eebc425e9df8cc2d3
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-19 14:36:48 -0700

    fetching data from the weather api

--- App.js
+++ App.js
@@ -7,22 +7,42 @@ import { WEATHER_API_KEY } from '@env'
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 const App = () => {
   const[loading, setLoading] = useState(true);
-  const [location, setLocation] = useState(null);
+  // const [location, setLocation] = useState(null);
   const [error, setError] = useState(null)
-  console.log(WEATHER_API_KEY);
+  const[weather, setWeather] = useState([]);
+  const[lat, setLat] = useState([])
+  const[lon, setLon] = useState([])
+
+  const fetchWeatherData = async() => {
+
+    try {
+      const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+      const data = await res.json();
+      setWeather(data);
+    } catch (error) {
+      setError('Could not fetch weather');
+    } finally{
+      setLoading(false);
+       
+    }
+     
+  }
+  
   useEffect(() => {
      (async() => {
-       let {satus} = await Location.requestForegroundPermissionsAsync()
+       let {status} = await Location.requestForegroundPermissionsAsync()
        if(status !== 'granted'){
           setError('permission to access location was denied')
           return
        }
        let location = await Location.getCurrentPositionAsync({})
-       setLocation(location)
-     })
-  }, [])
-  if(location){
-    console.log(location);
+       setLat(location.coords.latitude)
+       setLon(location.coords.longitude)
+       await fetchWeatherData()
+     })()
+  }, [lat, lon])
+  if(weather){
+    console.log(weather);
   }
   if (loading){
     return (
