b5f693aba4836a09a911eec2695b1510de0bc099
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-19 14:20:16 -0700

    adding the weather api to a an env file

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import { View, ActivityIndicator, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
 import * as Location from 'expo-location'
-import { TEST_KEY } from '@env'
+import { WEATHER_API_KEY } from '@env'
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 const App = () => {
   const[loading, setLoading] = useState(true);
   const [location, setLocation] = useState(null);
   const [error, setError] = useState(null)
-  console.log(TEST_KEY);
+  console.log(WEATHER_API_KEY);
   useEffect(() => {
      (async() => {
        let {satus} = await Location.requestForegroundPermissionsAsync()
