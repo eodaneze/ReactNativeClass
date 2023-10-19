cd5b63bb87b88b264d8404e8c0ee7a661efed1d6
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-19 14:15:00 -0700

    adding an env file

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, ActivityIndicator, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
+import * as Location from 'expo-location'
+import { TEST_KEY } from '@env'
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 const App = () => {
   const[loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null)
+  console.log(TEST_KEY);
+  useEffect(() => {
+     (async() => {
+       let {satus} = await Location.requestForegroundPermissionsAsync()
+       if(status !== 'granted'){
+          setError('permission to access location was denied')
+          return
+       }
+       let location = await Location.getCurrentPositionAsync({})
+       setLocation(location)
+     })
+  }, [])
+  if(location){
+    console.log(location);
+  }
   if (loading){
     return (
       <View style={styles.container}>
           <ActivityIndicator size={'large'} color={'blue'}/>
       </View>
     )
-  }
+   }
   return(
     <NavigationContainer>
        <Tabs />
